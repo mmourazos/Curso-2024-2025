6d38810fb587a1caaae0c4742f1034922b286381
mmourazos <77229234+mmourazos@users.noreply.github.com>
2025-02-21 19:35:59 +0100

    Corrección tarea ASGBD Tarea 3 + apuntes proced. almacen.

--- ASGBD/TareaUD3_A1.sql
+++ ASGBD/TareaUD3_A1.sql
@@ -1,18 +1,18 @@
-DROP PROCEDURE IF EXISTS Baloncesto.TAREAUD3;
+DROP PROCEDURE IF EXISTS Baloncesto.TAREAUD3_A1;
 
-- cambio
+DELIMITER # -- Establecemos un valor de delimitador distinto al punto y coma para poder incluirlos en el cuerpo del procedimiento. En este caso, usaremos el símbolo "#".
 
-delimiter #
-
-CREATE DEFINER='limitado'@'localhost' PROCEDURE Baloncesto.TAREAUD3()
+CREATE DEFINER='limitado'@'localhost' PROCEDURE Baloncesto.TAREAUD3_A1()
 SQL SECURITY DEFINER
 -- Puesto que lo vamos a ejecutar como el usuario 'limitado'@'localhost', debemos asegurarnos de que tiene los permisos necesarios para:
 -- * Ejecutar el procedimiento: GRANT EXECUTE ON PROCEDURE Baloncesto.TAREAUD3 TO 'limitado'@'localhost'.
 -- * Consultar las tablas necesarias: 
 --   * GRANT SELECT ON Baloncesto.jugadores TO 'limitado'@'localhost'; y GRANT SELECT ON Baloncesto.clases TO 'limitado'@'localhost';.
 --   * O bien simplemente: GRANT SELECT ON Baloncesto.* TO 'limitado'@'localhost`;
+
 BEGIN
 
+  -- Declaramos las variables que vamos a utilizar:
   DECLARE gr1, gr2 VARCHAR(20); -- Para almacenar los nombre de los grupos "Clases.grupo".
   DECLARE pt1, pt2 INT; -- Para almacenar la suma de las puntuaciones "Jugadores.tantos_marcados".
 
@@ -22,24 +22,24 @@ BEGIN
   WHERE j.clase = c.codigo GROUP BY j.clase ORDER BY suma DESC;
 
   -- Ahora accederemos únicamente a los dos primeros valores del cursor.
-  OPEN jugador_grupo;
+  OPEN jugador_grupo; -- Abrimos el cursor y hemos de cerrarlo al finalizar.
   -- Tomamos el primer valor del cursor.
   -- Fetch hace que el cursor avance al siguiente valor.
   FETCH jugador_grupo INTO gr1, pt1;
   -- Tomamos el siguiente valor del cursor.
   FETCH jugador_grupo INTO gr2, pt2;
   
-  CLOSE jugador_grupo;
+  CLOSE jugador_grupo; -- Cerramos el cursor.
 
   -- Añadimos 2 a la puntuación de la clase para el grupo gr1.
-  -- UPDATE clases SET puntuacion = puntuacion + 2 WHERE grupo = gr1;
+  UPDATE clases SET puntuacion = puntuacion + 2 WHERE grupo = gr1;
 
   -- Añadimos 1 a la puntuación de la clase para el grupo gr2.
-  -- UPDATE clases SET puntuacion = puntuacion + 1 WHERE grupo = gr2;
+  UPDATE clases SET puntuacion = puntuacion + 1 WHERE grupo = gr2;
  
   SELECT gr1 AS "grupo 1", pt1 AS "puntos 1";
   SELECT gr2 AS "grupo 2", pt2 AS "puntos 2";
 
 END #
 
-delimiter ;
+DELIMITER ; -- Restauramos el delimitador por defecto.
